22f2b40c09669e8868c74817ad25049e0b7b124f
Hamilton Gomes <hamilton.gomes8@hotmail.com>
2022-10-07 01:49:23 -0300

    problemas no do-while

--- src/descontoCombustivel.java
+++ src/descontoCombustivel.java
@@ -7,38 +7,77 @@ public class descontoCombustivel {
         DecimalFormat df = new DecimalFormat(".00");
 
         String resposta;
-        double litroCombustivel, etanol, gasolina;
+        int resposta2;
+        double litroCombustivel, etanol, gasolina, valor, desconto;
 
-        System.out.println("Qual tipo de combustível deseja comprar? (E/G)");
+        System.out.println("Qual tipo de combustível deseja comprar? (Etanol ou Gasolina)");
         resposta = scan.next();
-        while (! resposta.equalsIgnoreCase("e") && ! resposta.equalsIgnoreCase("g")){
+        while (!resposta.equalsIgnoreCase("etanol") && !resposta.equalsIgnoreCase("gasolina")) {
             System.out.println("Dados inválidos, por favor insira se deseja abastecer com gasolina (G) ou etanol (E).");
             resposta = scan.next();
         }
 
-        System.out.println("Deseja comprar quantos litros?");
-        litroCombustivel = scan.nextDouble();
+        do {
+            System.out.println("Digite o número que corresponde ao seu interesse: ");
+            System.out.println("1 - Abastecer informando, em litros, a quantidade de combustível;");
+            System.out.println("2 - Abastecer informando o saldo que será convertido em combustível;");
+            System.out.println("3 - Cancelar operação.");
+            resposta2 = scan.nextInt();
+        } while (resposta2 == 1 || resposta2 != 2 || resposta2 != 3);
 
-        // incrementar função para calcular a quantidade de litros pelo valor que o usuário deseja comprar
+        if (resposta2 == 1) {
+            System.out.println("Deseja comprar quantos litros?");
+            litroCombustivel = scan.nextDouble();
 
-        if (resposta.equalsIgnoreCase("e")) {
-            if (litroCombustivel <= 20) {
-                etanol = (3.24 * 0.97) * litroCombustivel;
-                System.out.println("O total é de R$ " + df.format(etanol));
-            } else if (litroCombustivel > 20) {
-                etanol = (3.24 * 0.95) * litroCombustivel;
-                System.out.println("O total é de R$ " + df.format(etanol));
+            if (resposta.equalsIgnoreCase("etanol")) {
+                if (litroCombustivel <= 20) {
+                    etanol = (3.24 * 0.97) * litroCombustivel;
+                    System.out.println("O total é de R$ " + df.format(etanol));
+                } else if (litroCombustivel > 20) {
+                    etanol = (3.24 * 0.95) * litroCombustivel;
+                    System.out.println("O total é de R$ " + df.format(etanol));
+                }
+            } else if (resposta.equalsIgnoreCase("gasolina")) {
+                if (litroCombustivel <= 20) {
+                    gasolina = (4.02 * 0.96) * litroCombustivel;
+                    System.out.println("O total é de R$ " + df.format(gasolina));
+                } else if (litroCombustivel > 20) {
+                    gasolina = (4.02 * 0.94) * litroCombustivel;
+                    System.out.println("O total é de R$ " + df.format(gasolina));
+                }
+            } else {
+                System.out.println("Valores inválidos.");
             }
-        } else if (resposta.equalsIgnoreCase("g")) {
-            if (litroCombustivel <= 20) {
-                gasolina = (4.02 * 0.96) * litroCombustivel;
-                System.out.println("O total é de R$ " + df.format(gasolina));
-            } else if (litroCombustivel > 20) {
-                gasolina = (4.02 * 0.94) * litroCombustivel;
-                System.out.println("O total é de R$ " + df.format(gasolina));
+
+        } else if (resposta2 == 2) {
+            System.out.println("Qual valor deseja abastecer?");
+            valor = scan.nextDouble();
+
+            if (resposta.equalsIgnoreCase("etanol")) {
+                litroCombustivel = (valor / 3.24);
+
+                if (litroCombustivel <= 20) {
+                    desconto = valor * 0.97;
+                    System.out.println("Você abastece " + litroCombustivel + " e ainda economiza R$ " + desconto);
+                } else if (litroCombustivel > 20) {
+                    desconto = valor * 0.95;
+                    System.out.println("Você abastece " + litroCombustivel + " e ainda economiza R$ " + desconto);
+                }
+            } else if (resposta.equalsIgnoreCase("gasolina")) {
+                litroCombustivel = (valor / 4.02);
+
+                if (litroCombustivel <= 20) {
+                    desconto = valor * 0.96;
+                    System.out.println("Você abastece " + litroCombustivel + " e ainda economiza R$ " + desconto);
+                } else if (litroCombustivel > 20) {
+                    desconto = valor * 0.94;
+                    System.out.println("Você abastece " + litroCombustivel + " e ainda economiza R$ " + desconto);
+                }
             }
+        } else if (resposta2 == 3) {
+            System.out.println("Obrigado pelo contato, volte sempre!");
         } else {
-            System.out.println("Valores inválidos.");
+            System.out.println("Valores inválidos, digite ");
         }
 
     }

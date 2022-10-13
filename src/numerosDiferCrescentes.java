6b81cea5beb31440450887a8b8ccbbd26b6f2479
Hamilton Gomes <hamilton.gomes8@hotmail.com>
2022-10-13 03:58:04 -0300

    código incompleto ainda

--- src/numerosDiferCrescentes.java
+++ src/numerosDiferCrescentes.java
@@ -4,32 +4,47 @@ public class numerosDiferCrescentes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a, b, c;
-        System.out.println("Escreva o primeiro valor: ");
-        a = scan.nextInt();
-        System.out.println("Escreva o primeiro valor: ");
-        b = scan.nextInt();
-        System.out.println("Escreva o primeiro valor: ");
-        c = scan.nextInt();
+
+        do {
+            System.out.println("Insira três números distintos abaixo.");
+            System.out.println("Escreva o primeiro valor: ");
+            a = scan.nextInt();
+            System.out.println("Escreva o segundo valor: ");
+            b = scan.nextInt();
+            System.out.println("Escreva o terceiro valor: ");
+            c = scan.nextInt();
+        } while (a == b || b == c || a == c);
 
         if (a > b) {
             if (b > c) {
                 System.out.println(a + "; " + b + "; " + c);
-            } else if (c > b && a > c) {
-                System.out.println(a + "; " + c + "; " + b);
+            } else if (a > c) {
+                System.out.println();
             }
-        } else if (b > a) {
-            if (a > c) {
-                System.out.println(b + "; " + a + "; " + c);
-            } else if (c > a && b > c) {
-                System.out.println(b + "; " + c + "; " + a);
-            }
-        } else if (c > a) {
-            if (a > b) {
-                System.out.println(c + "; " + a + "; " + b);
-            } else if (b > a && c > b) {
-                System.out.println(c + "; " + b + "; " + a);
-            }
-        }
+
+//            if (a > c && c > b) {
+//                System.out.println(a + "; " + c + "; " + b);
+//            }
+//        }
+//
+//        else if (b > c) {
+//            if (c > a) {
+//                System.out.println(b + "; " + c + "; " + a);
+//            } if (a > c && b > a) {
+//                System.out.println(b + "; " + c + "; " + a);
+//            }
+//        }
+//        else if (c > b) {
+//            if (a > b) {
+//                System.out.println(c + "; " + a + "; " + b);
+//            } else if (b > a && c > b) {
+//                System.out.println(c + "; " + b + "; " + a);
+//            }
+//        }
+//
+//        else{
+//            System.out.println("Dados inválidos.");
+//        }
 
     }
 }
